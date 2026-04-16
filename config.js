5da0367331b6c98ca380df2e82fdb4017d7916fb
kgw279 <kgw279@gmail.com>
2026-04-15 20:32:02 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// 1. Get your CLIENT_ID and API_KEY from the Google Cloud Console
+// 2. The SPREADSHEET_ID is the long string in your Sheet's URL
+const CONFIG = {
+    CLIENT_ID: '323221869326-en1936d6cg90qcmmm1gp2rpoo7tu50ak.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyDte0cZEz2KjHVKFnKn6qQfLAhgvYFoqdg',
+    SPREADSHEET_ID: '1yhe11cWmRk5jHeieWkv14X0gFLlSG2fMiKmYggZmKGE',
+    DEFAULT_RANGE: 'Log!B:E'
+};
